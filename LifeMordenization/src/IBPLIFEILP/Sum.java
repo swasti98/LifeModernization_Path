807eca68668c7175d3d9758ca20ce1e6c485051a
swast <swast@SWASTI>
2024-09-22 23:02:22 +0530

    Sum Of a No.

---
+++ LifeMordenization/src/IBPLIFEILP/Sum.java
@@ -0,0 +1,15 @@
+package IBPLIFEILP;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 10;
+		int b = 89;
+		int sum = a+b;
+		System.out.println("sum="+sum);
+	}
+	
+}
